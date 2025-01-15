9852c53a56a08d205cf8b8798bbe39ccd90e5a8b
PAO <4A61C003@stust.edu.tw>
2025-01-15 11:22:27 +0800

    新增會員資料庫修改執行檔

---
+++ .gitignore/config.php
@@ -0,0 +1,10 @@
+<?php
+
+const DB_HOST = '127.0.0.1';
+const DB_USER = 'root';
+const DB_PASS = 'P@ssw0rdo1';
+const DB_NAME = 'gym_database';
+const DB_PORT = 3306;
+
+
+const PATH_BASE = 'gym-report-main';
