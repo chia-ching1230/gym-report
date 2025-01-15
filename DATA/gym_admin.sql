d3c9e9f7e79b13ef07e90b1c6aec22e05248f4ed
PAO <4A61C003@stust.edu.tw>
2025-01-15 21:29:40 +0800

    修改欄位admin_password_hash及儲存雜湊碼、login-api(admin_password_hash)

--- DATA/gym_admin.sql
+++ DATA/gym_admin.sql
@@ -4,20 +4,20 @@ create table gym_admin (
 admin_id int auto_increment primary key,
 admin_name varchar(50) NOT NULL,
 email varchar(255) NOT NULL UNIQUE,
-admin_password varchar(255) NOT NULL,
+admin_password_hash varchar(255) NOT NULL,
 admin_role int NOT NULL,
 admin_code VARCHAR(10) UNIQUE,  
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  
 );
 
-insert into gym_admin(admin_name, email, admin_password,admin_role,admin_code)
+insert into gym_admin(admin_name, email, admin_password_hash,admin_role,admin_code)
 values
-("Owen","dtes95105@gmail.com","P@ssw0rdo0",1,"A2501"),
-("Pao","mwz236268@gmail.com","P@ssw0rdo1",2,"A2502"),
-("Ching","s22h20@gmail.com","P@ssw0rdo2",2,"A2503");
+("Owen","dtes95105@gmail.com","$2y$10$pLXg5uJbrJ6Kzu3GL49t/OGD2SzRBYCLfSyTsgNoXriW66qYKzeIi",1,"A2501"),
+("Pao","mwz236268@gmail.com","$2y$10$1xD0C2DglUWwfrw1jSRLHeH5G7UqGuF/pjAxLqOpjziC0e7ckR6km",2,"A2502"),
+("Ching","s22h20@gmail.com","$2y$10$V07IfoGb9jBDCkv9S/Y7tunjfbxefqlxzFH5G3REEYPHKfWZgLjQa",2,"A2503");
 -- ("Yang","abcd6968688@gmail.com","P@ssw0rdo3",2,"A2504");
  select * from gym_admin;
 
 
--- drop table gym_admin;
+--  drop table gym_admin;
